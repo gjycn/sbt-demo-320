a04bff4f6f97b1dba0ddc43d9bd584393d9486d7
Junyang <gjy.cn@outlook.com>
2023-12-23 01:38:52 +0800

    Initial commit

---
+++ src/test/java/com/example/sbtdemo320/SbtDemo320ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtdemo320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtDemo320ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
